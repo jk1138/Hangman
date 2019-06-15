b592defc6ddf5642f44d265c6eb18511f58209d4
jyk <47995007+jk1138@users.noreply.github.com>
2019-06-15 13:29:26 -0400

    Colin's game.js script

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+var words = ["first", "second", "third", "fourth", "fifth"]
+
+var lettersGuess = [];
+var guessesRemaining = [];
+var wins = 0;
+var losses = 0;
+var currentWord = "";
+var userGuess = "";
+
+function gameSetup(){
+
+       currentWord = words[Math.floor(Math.random() * words.length)];
+
+       alert(currentWord);
+
+   }
+
+
+
+
+function gameReset(){
+
+}
+
+function letterCheck(letter){
+if(){
+
+}else{
+}
+
+}
+
+function gameWon(){
+
+}
+
+gameSetup();
+
+// on key press function here //
+
+letterCheck(lettetKey);
+Message Input
+
+
+Message Colin McMahon
